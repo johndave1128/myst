d72dc5541e21ec4559aed7ecdb323afc8c188b2c
myst <myst@grandchase>
2023-07-10 16:38:47 +0800

    100% done mobile view

--- script.js
+++ script.js
@@ -5,9 +5,8 @@ window.addEventListener('load', function() {
   loadingScreen.style.display = 'none';
   content.style.display = 'block';
 });
-//animation
-var scrollOffset = 50;
 // Add smooth scroll behavior to navigation links
+var scrollOffset = 50;
 var navLinks = document.querySelectorAll('nav a');
 navLinks.forEach(function(link) {
   link.addEventListener('click', function(event) {
@@ -21,12 +20,12 @@ navLinks.forEach(function(link) {
     });
   });
 });
-
 // top button
 let mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if ((document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) && window.innerWidth > 600)
+  {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
@@ -37,6 +36,18 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+// topnav and bottom-nav
+let topNav = document.getElementById("top-nav");
+let bottomNav = document.getElementById("bottom-nav");
+
+if (window.innerWidth < 600) {
+  // Mobile view
+  topNav.style.display = "none";  
+} else {
+  // Desktop view
+  bottomNav.style.display = "none";
+}
+// redirect to store
 function redirectToStore() {
   var userAgent = navigator.userAgent || navigator.vendor || window.opera;
   
@@ -52,3 +63,12 @@ function redirectToStore() {
     alert('Unsupported device. Please visit the appropriate store manually.');
   }
 }
+
+// const list = document.querySelectorAll('list');
+// function activeLink(){
+//   list.forEach((item) => 
+//     item.classList.remove('active'));
+//     this.classList.add('active');
+//   }
+//   list.forEach((item) =>
+//   item.addEventListener('click', activeLink));
